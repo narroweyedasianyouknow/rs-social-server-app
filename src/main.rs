10a6bdf1e036537fbb1f738173ed2c855bdd24ba
altaev <altaev@clout.one>
2023-07-22 16:15:41 +0300

    feature Image Uploading

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 pub mod db;
 
-use actix_web::{web, App, HttpServer};
-use db::{get_db, create_unique_index};
+use actix_web::{
+    web::{post, resource, Data},
+    App, HttpServer,
+};
+use db::{create_unique_index, get_db};
 use dotenv::dotenv;
-use routes::authorization::{registration, login};
+use routes::{
+    authorization::{login, registration},
+    posts::create_post,
+    uploader::image_upload,
+};
 mod utils {
     pub mod filter_json;
 }
 use types::{collections::COLLECTION_NAMES, user::StoredUserType};
 mod routes {
     pub mod authorization {
-        pub mod registration;
-        pub mod password_utils;
         pub mod login;
+        pub mod password_utils;
+        pub mod registration;
+    }
+    pub mod posts {
+        pub mod create_post;
+    }
+    pub mod uploader {
+        pub mod image_upload;
     }
 }
 mod types {
     pub mod collections;
+    pub mod post;
     pub mod user;
 }
 
 use std::env;
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -29,14 +42,17 @@ async fn main() -> std::io::Result<()> {
     let url = &env::var("BACKEND_URI").expect("BACKEND_URI not set");
     // Подключение к базе данных
     let db = get_db().await;
-    let users_collection: mongodb::Collection<StoredUserType> = db.collection(COLLECTION_NAMES.users.as_str());
+    let users_collection: mongodb::Collection<StoredUserType> =
+        db.collection(COLLECTION_NAMES.users.as_str());
     let _ = create_unique_index(&users_collection).await;
     HttpServer::new(move || {
         App::new()
-            .route("/registration", web::post().to(registration::main))
-            .route("/login", web::post().to(login::main))
+            .service(resource("/upload").route(post().to(image_upload::upload_file)))
+            .route("/registration", post().to(registration::main))
+            .route("/post", post().to(create_post::main))
+            .route("/login", post().to(login::main))
             // Добавьте свои маршруты и обработчики запросов здесь
-            .app_data(web::Data::new(db.clone())) // Добавьте доступ к базе данных в состояние приложения
+            .app_data(Data::new(db.clone())) // Добавьте доступ к базе данных в состояние приложения
     })
     .bind(url)?
     .run()
--- src/types/collections.rs
+++ src/types/collections.rs
@@ -3,10 +3,12 @@ use lazy_static::lazy_static;
 #[derive(Debug)]
 pub struct Collections {
     pub(crate) users: String, // Изменили тип поля на String
+    pub(crate) post: String, // Изменили тип поля на String
 }
 
 lazy_static! {
     pub static ref COLLECTION_NAMES: Collections = Collections {
         users: "users".to_string(),
+        post: "post".to_string(),
     };
 }
